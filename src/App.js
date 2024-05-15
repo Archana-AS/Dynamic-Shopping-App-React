5c4224e492d83f3bc523fa7b5c39668e627af50e
Archana-AS <archanaas2321@gmail.com>
2024-05-16 01:17:01 +0530

    routing done

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import Navbar from './components/Navbar';
 import Search from './components/Search';
 import Add from './components/Add';
 import Home from './components/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div >
       
-      <Home/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/add' element={<Add/>}/>
+          <Route path='/search' element={<Search/>}/>
+        </Routes>
+      </BrowserRouter>
       
 
     </div>
